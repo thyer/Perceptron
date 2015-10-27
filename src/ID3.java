5797a4082d9b7629e69440c3f02bda185d98884c
Trenton Adam Hyer <thyer@byu.edu>
2015-10-27 11:18:44 -0600

    Updating gitignore for .class files

--- src/ID3.java
+++ src/ID3.java
@@ -1,9 +1,15 @@
 import java.util.Map;
+import java.util.Random;
 import java.util.TreeMap;
 
 public class ID3 extends SupervisedLearner{
 
 	private ID3Tree tree;
+	private Random rand;
+	
+	public ID3(Random rand){
+		this.rand = rand;
+	}
 	
 	public void train(Matrix features, Matrix labels) throws Exception {
 		tree = new ID3Tree(features, labels);
