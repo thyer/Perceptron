f8230b23ba74f3de83936771cf573f86d066fa39
Trenton Adam Hyer <thyer@byu.edu>
2015-11-13 23:55:24 -0700

    KNN changes finished

--- src/InstanceBasedLearner.java
+++ src/InstanceBasedLearner.java
@@ -7,7 +7,8 @@ public class InstanceBasedLearner extends SupervisedLearner {
 	private Random rand;
 	private Matrix mxFeatures;
 	private Matrix mxLabels;
-	private final int K = 5;
+	private double[] colAverage = null;
+	private final int K = 9;
 	private final boolean DISTANCE_WEIGHTING = true;
 	private final boolean REGRESSION = false;
 
@@ -18,14 +19,106 @@ public class InstanceBasedLearner extends SupervisedLearner {
 
 	@Override
 	public void train(Matrix features, Matrix labels) throws Exception {
-		this.mxFeatures = features;
-		this.mxLabels = labels;
-		this.mxFeatures.shuffle(this.rand, this.mxLabels);
+		int rowsToKeep = (int) (1 * features.rows());
+		features.shuffle(this.rand, labels);
+		this.mxFeatures = new Matrix(features, 0, 0, rowsToKeep, features.cols());
+		this.mxLabels = new Matrix(labels, 0, 0, rowsToKeep, labels.cols());
+		this.repairMatrix(this.mxFeatures);
+		//this.reduceRows(this.mxFeatures, this.mxLabels);
+	}
+
+	private void reduceRows(Matrix features, Matrix labels) throws Exception {
+		System.out.println("At beginning, there are " + features.rows() + " instances");
+		double validationSize = 0.1;
+		int validationRows = (int) (features.rows() * validationSize);
+		Matrix mxValidationFeatures = new Matrix(features, 0, 0, validationRows, features.cols());
+		Matrix mxToPruneFeatures = new Matrix(features, validationRows, 0, features.rows()-validationRows, features.cols());
+		Matrix mxValidationLabels = new Matrix(labels, 0, 0, validationRows, labels.cols());
+		Matrix mxToPruneLabels = new Matrix(labels, validationRows, 0, labels.rows()-validationRows, labels.cols());
+		for(int i = 0; i < mxToPruneFeatures.rows(); ++i){
+			this.mxFeatures = mxToPruneFeatures;
+			this.mxLabels = mxToPruneLabels;
+			if(!this.assessInstance(i, mxValidationFeatures, mxValidationLabels)){
+				Matrix tempFeatures = new Matrix(mxToPruneFeatures, 0, 0, i, mxToPruneFeatures.cols());
+				Matrix tempLabels = new Matrix(mxToPruneLabels, 0, 0, i, 1);
+				if(i!=mxToPruneFeatures.rows()-1){
+					tempFeatures.add(mxToPruneFeatures, i+1, 0, mxToPruneFeatures.rows()-i-1);
+					tempLabels.add(mxToPruneLabels, i+1, 0, mxToPruneFeatures.rows()-i-1);
+				}
+				i--;
+				mxToPruneFeatures = tempFeatures;
+				mxToPruneLabels = tempLabels;
+			}
+		}
+		this.mxFeatures = mxToPruneFeatures;
+		this.mxLabels = mxToPruneLabels;
+		this.mxFeatures.add(mxValidationFeatures, 0, 0, mxValidationFeatures.rows());
+		this.mxLabels.add(mxValidationLabels, 0, 0, mxValidationLabels.rows());
+		System.out.println("At end, there are " + mxFeatures.rows() + " instances");
+		
+	}
+
+	private boolean assessInstance(int instance, Matrix mxValidationFeatures, Matrix mxValidationLabels) throws Exception {
+		//tempFeatures,tempLabels are the vectors without the given instance
+		Matrix tempFeatures = new Matrix(mxFeatures, 0, 0, instance, mxFeatures.cols());
+		Matrix tempLabels = new Matrix(mxLabels, 0, 0, instance, 1);
+		if(instance!=mxFeatures.rows()-1){
+			tempFeatures.add(mxFeatures, instance+1, 0, mxFeatures.rows()-instance-1);
+			tempLabels.add(mxLabels, instance+1, 0, mxFeatures.rows()-instance-1);
+		}
+		
+		//get accuracy with the instance
+		double accuracy1 = 0;
+		for(int i = 0; i < mxValidationFeatures.rows(); ++i){
+			double prediction[] = {0.0};
+			this.predict(mxValidationFeatures.row(i), prediction);
+			if(prediction[0] == mxValidationLabels.row(i)[0]){
+				accuracy1 += 1.0/mxLabels.rows();
+			}
+		}
+		
+		//get accuracy without the instance
+		double accuracy2 = 0;
+		this.mxFeatures = tempFeatures;
+		this.mxLabels = tempLabels;
+		for(int i = 0; i < mxValidationFeatures.rows(); ++i){
+			double prediction[] = {0.0};
+			this.predict(mxValidationFeatures.row(i), prediction);
+			if(prediction[0] == mxValidationLabels.row(i)[0]){
+				accuracy2 += 1.0/mxLabels.rows();
+			}
+		}
+		//System.out.println("Accuracy with: " + accuracy1 + ", accuracy without: " + accuracy2);
+		return accuracy1 >= accuracy2;
+	}
+
+	private void repairMatrix(Matrix mx) {
+		colAverage = new double[mx.cols()];
+		int repaired = 0;
+		for(int j = 0; j < mx.cols(); ++j){
+			colAverage[j] = mx.columnMean(j);
+			for(int i = 0; i < mx.rows(); ++i){
+				if(mx.get(i, j) == Matrix.MISSING){
+					mx.set(i, j, colAverage[j]);
+					repaired++;
+				}
+			}
+		}
+		System.out.println("Repaired " + repaired + " cells");	
+		System.out.println("colAverage: ");
+		for (double d : colAverage){
+			System.out.println("\t" + d);
+		}
 	}
 
 	@Override
 	public void predict(double[] features, double[] labels) throws Exception {
 		//calculate and order distances to each instance
+		for(int j = 0; j < features.length; ++j){
+			if(colAverage != null && features[j] == Matrix.MISSING){
+				features[j] = this.colAverage[j];
+			}
+		}
 		PriorityQueue<Point> bestK = new PriorityQueue<Point>();
 		for(int i = 0; i < mxFeatures.rows(); ++i){
 			double dist = calcDistance(features, mxFeatures.row(i));
